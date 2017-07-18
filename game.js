0d436766706e520f8827b3d3a32879e5a296cd2c
mhoppenrath <mmhoppenrath@gmail.com>
2017-07-18 18:35:49 -0500

    it looks good static wise

--- game.js
+++ game.js
@@ -1,61 +1,57 @@
-// java is the last to initalize on the page
-//initalizing global variables that will be used throughout.
-	var min = 0;
-	var magicMax = 100;
-	var crystalMax=10;
-	var yourScore=0
-	var wins=0;
-	var losses=0;
-	var images = ['assets/dimond.png', 'assets/emerald.png', 'assets/ruby.png', 'assets/saphire.png'];
+$(document).ready(function() {
+	//creates some global variables for use
+
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	var crystalMax = 10;
+	var max=100;
 
 
-	// this function is to generate the magic number
+	//links and writes out the variables below
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	//creates an aray with the value to images 
+	images = ['assets/ruby.png','assets/saphire.png','assets/dimond.png','assets/emerald.png'];
+
+	//our two number generators on for the computer's guess and one for the crystals values
 	function magicGenerator (){
-		var random = Math.floor(Math.random() * (magicMax - min + 1) + min);
+		var random = Math.floor(Math.random() * (max+ 1));
 		return random;
 	}
-
-	//This is to make the first Magic number
-	var magicNumber = magicGenerator();
-	console.log(magicNumber)
-
-
-	//function Creates the value of the crystal Values
-	function crystalGenerator (){
-		var random = math.floor(math.random() * (crystalMax - min) + min);
+	function crystalNumber (){
+		var random = Math.floor(Math.random() * (crystalMax));
 		return random;
 	}
+	var test1 = magicGenerator();
+	var test2 = crystalNumber();
+	console.log(test1 + " " + test2)
+	//this function will exist in the Crystal Generator to check if the value has been used
 
-	// creates a function that creates images as objects
-	var createImage = function(link) {
-  		var imageCrystal   = $("<img>");
-  		imageCrystal.addClass("gem");
-  		imageCrystal.attr("src", link);
-  		var meth = crystalGenerator
-  		imageCrystal.attr("value", meth)
-  		console.log(imageCrystal.src);
-  		console.log(imageCrystal.item);
-  		$("#crystalHolder").append(imageCrystal);
-  	};
+	//this will create the the cryistals to materialize on the page
+	function crystalGenerator() {
+		var values = [];
+			while(values.length <4){
+				var value = Math.ceil(Math.random()*12)
+				var found = false;
+				for (var i = values.length - 1; i >= 0; i--) {
+					if (values[i] === value){
+						found =true
+					}
+				}
+		  	if(!found)values[values.length]=value;
+			}
+		console.log(values);
+		for (var i = 0; i < values.length; i++) {
+			var imageCrystal = $('<img>');
+			imageCrystal.attr('data-number', value[i]);
+			imageCrystal.attr('src', images[i]);
+			imageCrystal.attr('alt', 'crystals');
+			imageCrystal.addClass('crystalImage');
+			$('#crystalHolder').append(imageCrystal);
+		}
 
-
-  	$(".gem").on("click", function() {
-  		yourScore = yourScore + parseInt($(this).value('num'))
-  		if (yourScore === magicNumber) {
-  			wins ++;
-  			yourScore = 0;
-  			//crystals empty
-  		}
-  		else if ( yourScore > magicGenerator){
-  			losses ++;
-  			yourScore = 0;
-  			//empy cristal
-  		}
-  	})
-	//the specific images
-
-
-	//this populates the crystalHolder row with 4 seperate images
-
-
+	}
+	crystalGenerator();
+});
 
